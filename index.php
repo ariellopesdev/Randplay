0e50fe408b329c291b4b25273adbb7a785a7f6bf
ariellopesdev <lubyel.lopes@gmail.com>
2023-12-18 22:17:43 -0300

    Criação do Site
    Criado estrutura simples do site, criado html da home do site.

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Randplay</title>
+    <link rel="stylesheet" href="estilo/styles.css">
+</head>
+
+<body>
+    <header>
+        <header>
+            <img src="" alt="Logo RandPlay">
+            <div class="nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Notícias</li>
+                    <li>Sobre Nós</li>
+                    <li>Nosso Time</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+            <input type="search" name="" id="">
+            <img src="" alt="Ícone More">
+        </header>
+        <main>
+            <p>- </p>
+            <h1>Randplay - Futebol <br> Criador de Campeonatos</h1>
+            <p>Comece agora.</p>
+            <button class="login">
+                Cadastre-se
+            </button>
+            <button class="sign">
+                Login
+            </button>
+        </main>
+    </header>
+    <main>
+        <header>
+            <div class="slider">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </header>
+        <main>
+            <article id="feature">
+                <h2>Nossos Recursos</h2>
+                <p>Você pode criar qualquer tipo de campeonato de futebol</p>
+                <button class="prev">
+                    << /button>
+                        <button class="next">></button>
+                        <div class="slider">
+                            <p>Copa do Mundo</p>
+                            <p>Campeonato Brasileiro</p>
+                            <p>Copa do Brasil</p>
+                            <p>Campeonato Argentino</p>
+                            <p>Copa Simón Bolívar</p>
+                            <p>Copa Federação Gaúcha de Futebol</p>
+                        </div>
+            </article>
+            <article id="service">
+                <h2>Nossos Serviços</h2>
+                <p>Alta capacidade de gerenciamento e controle de campeonatos e ligas.</p>
+                <button class="prev">
+                    < </button>
+                        <button class="next">></button>
+                        <div class="slider">
+                            <p>Painel de Controle</p>
+                            <p>Criação de Ligas, Copas e Campeonatos</p>
+                            <p>Inclusão de times, seleções e escudos</p>
+                            <p>Tabelas dinâmicas e de fácil visualização</p>
+                        </div>
+            </article>
+            <article id="news">
+                <h3>Notícias</h3>
+                <p>Notícias do mundo do futebol e sobre atualizações de nosso site.</p>
+                <figure>
+                    <img src="" alt="">
+                    <h4>Título da Notícia</h4>
+                    <hr>
+                    <i></i> Nov 22, 2023 <i></i> 1.2k <i></i> 245
+                </figure>
+                <figure>
+                    <img src="" alt="">
+                    <h4>Título da Notícia</h4>
+                    <hr>
+                    <i></i> Nov 22, 2023 <i></i> 1.2k <i></i> 245
+                </figure>
+                <figure>
+                    <img src="" alt="">
+                    <h4>Título da Notícia</h4>
+                    <hr>
+                    <i></i> Nov 22, 2023 <i></i> 1.2k <i></i> 245
+                </figure>
+            </article>
+            <article id="about">
+                <h2>Sobre Nós</h2>
+                <p>Conheça um pouco de nossa empresa.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi molestias quam quisquam alias aut temporibus, incidunt tenetur, beatae ipsam voluptates culpa eligendi omnis, maiores non accusantium. Asperiores earum cum nam?</p>
+            </article>
+            <article id="ourteam">
+                <h2>Nosso Time</h2>
+                <p>Conheça a equipe por trás do projeto Randplay.</p>
+                <button class="prev">
+                    < </button>
+                        <button class="next">></button>
+                        <div class="slider">
+                            <p>Painel de Controle</p>
+                            <p>Criação de Ligas, Copas e Campeonatos</p>
+                            <p>Inclusão de times, seleções e escudos</p>
+                            <p>Tabelas dinâmicas e de fácil visualização</p>
+                        </div>
+            </article>
+            <article id="contact">
+                <h3>Contato</h3>
+                <p>Contato com nossa equipe.</p>
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="email" name="" id="">
+                <input type="tel" name="" id="">
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <button type="submit">Envie Mensagem</button>
+                <i></i> Endereço <p>Rua Bartolomeu Lourenço de Gusmão, Hauer, Curitiba</p>
+                <i></i> Telefone <p>(41) 99139-3836</p>
+                <i></i> E-mail <p>ariel_lopescwb2017@hotmail.com</p>
+            </article>
+        </main>
+    </main>
+    <footer>
+        <img src="" alt="Logo Randplay">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto expedita similique, repellendus fugit dolor eos sequi impedit hic modi alias blanditiis quam aut! Dolore maxime dignissimos numquam ad earum obcaecati!</p>
+        <p>Follow Us</p>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <h4>Links Rápidos</h4>
+        <ul>
+            <li>Cadastre-se</li>
+            <li>Nosso Time</li>
+            <li>Sobre Nós</li>
+            <li>Ajuda</li>
+            <li>Contato</li>
+        </ul>
+        <h4>Tags Populares</h4>
+        <ul>
+            <li>Notícias</li>
+            <li>Campeonato</li>
+            <li>Liga</li>
+        </ul>
+        <h4>Nossos Serviços</h4>
+        <ul>
+            <li>Criar Campeonato</li>
+            <li>Editar Campeonato</li>
+            <li>Excluir Time</li>
+        </ul>
+        <p>Copyright - All Rights Reserved to ariellopesdev</p>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
